75a48bee9bca58dd670f9bbdbb58f3eae60c5050
hdk24 <hanantadk@gmail.com>
2017-04-26 18:06:28 +0700

    change method

--- project/controllers/index.py
+++ project/controllers/index.py
@@ -15,20 +15,20 @@ def hello():
 
 # get all language endpoint
 @app.route("/lang", methods=['GET'])
-def returnLang():
+def return_lang():
     return jsonify({entity_message: languages})
 
 
 # get based id /lang/[id]
 @app.route('/lang/<int:id>', methods=['GET'])
-def returnOne(id):
+def return_one(id):
     indexs = [ids for ids in languages if ids['id'] == id]  # return array
     return jsonify({entity_message: indexs})
 
 
 # add data param -> name
 @app.route('/lang', methods=['POST'])
-def addOne():
+def add_one():
     id_new = len(languages) + 1
     params_name = request.form['name']
     language = {'id': id_new, 'name': params_name}
